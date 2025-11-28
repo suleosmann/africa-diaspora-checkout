486d02fb77322bdf012eb799205a2fa44aa6a5dd
Suleiman Osman <suleosman73@gmail.com>
2025-11-28 15:50:53 +0300

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://js.paystack.co/v1/inline.js" defer></script>
+    <link rel="icon" type="image/png" href="/favicon.svg">
+
 
     <title inertia>Africa Diaspora Checkout</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
